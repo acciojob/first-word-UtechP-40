0886a4a7c1b530c3192ae9e9a0ec30a8c560ea76
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-08-26 20:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function firstWord(s) {
   // your code here
 	let xs = s.split(" ")
-	if (xs.length<=1) {
+	if (xs.length==1) {
 		return s
+	}else if(s.length==0){
+		return ''
 	}
 	return xs[0]
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
